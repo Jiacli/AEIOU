55a4a4caf752209c62c948e11f06362056b1e35c
Alexhxq <huangxq.alex@gmail.com>
2015-02-19 22:50:38 -0500

    version1.0

--- code/Xiaoqiu/parser.py
+++ code/Xiaoqiu/parser.py
@@ -1,8 +1,29 @@
 import os
 from nltk.parse import stanford
-os.environ['STANFORD_PARSER'] = '/Users/XiaoqiuHuang/Google_Drive/11-411/QAsystem/jars/'
-os.environ['STANFORD_MODELS'] = '/Users/XiaoqiuHuang/Google_Drive/11-411/QAsystem/jars/'
 
-parser = stanford.StanfordParser(model_path="/Users/XiaoqiuHuang/Google_Drive/11-411/QAsystem/jars/englishPCFG.ser.gz")
-sentences = parser.raw_parse_sents(("I want to go to the party tomorrow"))
-print sentences[0]
+class parser:
+	def __init__(self):
+		self.path = '/Users/XiaoqiuHuang/Google_Drive/11-411/AEIOU/code/Xiaoqiu/jars/'
+		os.environ['STANFORD_PARSER'] = self.path
+		os.environ['STANFORD_MODELS'] = self.path
+
+		self.parser = stanford.StanfordParser(model_path=self.path+"englishPCFG.ser.gz")
+
+	def generateWho(self, sents):
+		ques = []
+		sentences = self.parser.raw_parse_sents(sents)
+		#sentences[1].draw()
+		print sentences[1].subtrees()[0].label()
+		#self.dfs(sentences[0], ques)
+		print ques
+
+
+	def dfs(self, sentences, ques):
+		for subtree in sentences.subtrees():
+			ques.append(subtree.label())
+			self.dfs(subtree, ques)
+
+
+if __name__=="__main__":
+	model = parser()
+	model.generateWho(("USS Constitution is a wooden-hulled, three-masted heavy frigate of the U.S. Navy.","He also likes eating sausage, based on the assumption."))
