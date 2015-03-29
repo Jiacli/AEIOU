91fc27f885e74cef53349121c2325c3c0f3795a7
Jiachen Li <Jiacli@users.noreply.github.com>
2015-03-28 22:08:07 -0400

    Added html parser (to plain text)
    only include paragraph

---
+++ code/data/parse_html.py
@@ -0,0 +1,51 @@
+import os, sys
+import re
+
+
+def main(args):
+    input_path = sys.argv[1]
+    output_path = sys.argv[2]
+
+    file_list = list_all_file(input_path)
+
+    for filename in file_list:
+        print 'parsing file:', filename
+        parse_html_file(filename, output_path)
+    print 'done.'
+
+
+def parse_html_file(filename, output_dir):
+    title = ''
+    sent_list = []
+    with open(filename) as f:
+        for line in f.readlines():
+            if len(line) == 0:
+                continue
+            if line.startswith('<title>'):
+                tmp = re.sub('<[^>]+>', '', line.strip())
+                title = re.sub(r'\s+', '_', tmp)
+                print 'title:', title
+            elif line.startswith('<p>'):
+                line = re.sub('<[^>]+>', '', line.strip())
+                sent_list.append(line)
+            else:
+                continue
+    if not output_dir.endswith('/'):
+        output_dir += '/'
+    with open(output_dir + title, 'w') as g:
+        for line in sent_list:
+            g.write(line + '\n')
+
+
+def list_all_file(dir):
+    file_list = []
+    for root, dirnames, filenames in os.walk(dir):
+        for filename in filenames:
+            if 'DS_Store' in filename:
+                continue
+            file_list.append(os.path.join(root,filename))
+    return file_list
+
+
+if __name__ == '__main__':
+    main(sys.argv)
